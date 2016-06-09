bb502e8b46499db982ae1d470e291ac2d889fb03
datha2703 <datha_k@yahoo.co.in>
2016-06-09 10:24:53 +0000

    Initial version Kendo UI has below things - Splitter - List Box - Microsoft project

---
+++ scripts/dashboard.js
@@ -0,0 +1,17 @@
+﻿// http://stackoverflow.com/questions/1327756/can-you-have-multiple-document-readyfunction-sections
+$(document).ready(function () {
+    dashboardInit();
+    watchListInit();
+});
+
+// http://docs.telerik.com/kendo-ui/controls/layout/splitter/overview
+function dashboardInit() {
+    $("#horizontal").kendoSplitter({
+        panes: [
+            { collapsible: true, size: "20%" },
+            { collapsible: false },
+            { collapsible: true, size: "20%" }
+        ],
+        orientation: "horizontal"
+    });
+}
---
+++ scripts/watchList.js
@@ -0,0 +1,16 @@
+﻿function watchListInit() {
+    $("#dashboardWatchList").kendoListView({
+        dataSource: watchListItems,
+        pageable: true,
+        template: kendo.template($("#watch_list_item_template").html())
+    });
+}
+
+var watchListItems = new kendo.data.DataSource({
+    transport: {
+        read: {
+            url: "http://demos.kendoui.com/service/Products",
+            dataType: "jsonp"
+        }
+    }
+});
